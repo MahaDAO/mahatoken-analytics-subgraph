a69da24402cce8c80ec555bf0452cbccff12a68f
Steven Enamakel <enamakel@vezures.com>
2021-03-31 20:42:44 +0300

    chagned the block time start

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,6 +3,7 @@ import {
   EXP_18,
   fetchDayData,
   fetchFinalData,
+  ZERO_BI,
   fetchWallet,
   ONE_BI,
 } from "./helpers";
@@ -11,7 +12,7 @@ export function handleTransfer(event: Transfer): void {
   let timestamp = event.block.timestamp.toI32();
   let dayID = timestamp / 86400;
 
-  let finalData = fetchFinalData();
+  const finalData = fetchFinalData();
   const dayData = fetchDayData(dayID);
 
   // update balances
@@ -20,7 +21,7 @@ export function handleTransfer(event: Transfer): void {
   const val = event.params.value;
 
   const beforeFromBalance = from.balance;
-  const beforeToBalance = from.balance;
+  const beforeToBalance = to.balance;
 
   from.txCount = from.txCount.plus(ONE_BI);
   to.txCount = to.txCount.plus(ONE_BI);
@@ -29,7 +30,7 @@ export function handleTransfer(event: Transfer): void {
   to.balance = to.balance.plus(val);
 
   const afterFromBalance = from.balance;
-  const afterToBalance = from.balance;
+  const afterToBalance = to.balance;
 
   const minBal = EXP_18;
   if (beforeFromBalance.le(minBal) && afterFromBalance.gt(minBal))
@@ -38,18 +39,26 @@ export function handleTransfer(event: Transfer): void {
   if (beforeToBalance.le(minBal) && afterToBalance.gt(minBal))
     finalData.walletCount = finalData.walletCount.plus(ONE_BI);
 
-  if (beforeFromBalance.gt(minBal) && afterFromBalance.le(minBal))
+  if (
+    beforeFromBalance.gt(minBal) &&
+    afterFromBalance.le(minBal) &&
+    finalData.walletCount.notEqual(ZERO_BI)
+  )
     finalData.walletCount = finalData.walletCount.minus(ONE_BI);
 
-  if (beforeToBalance.gt(minBal) && afterToBalance.le(minBal))
+  if (
+    beforeToBalance.gt(minBal) &&
+    afterToBalance.le(minBal) &&
+    finalData.walletCount.notEqual(ZERO_BI)
+  )
     finalData.walletCount = finalData.walletCount.minus(ONE_BI);
-
-  from.save();
-  to.save();
 
   finalData.txCount = finalData.txCount.plus(ONE_BI);
   dayData.txCount = dayData.txCount.plus(ONE_BI);
   dayData.walletCount = finalData.walletCount;
 
+  from.save();
+  to.save();
+  finalData.save();
   dayData.save();
 }
